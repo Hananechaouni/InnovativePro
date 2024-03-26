e6038917dae6a79bc3f1a4121c60254d942b6ad0
Butrind <73846813+Butrind@users.noreply.github.com>
2024-03-26 16:44:22 +0100

    Update of the frontend, add button and modified app.component.ts for sending HTTP request to the proxay and handling the output

--- InnovativePro-app/src/app/app.component.ts
+++ InnovativePro-app/src/app/app.component.ts
@@ -1,13 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+ 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  providers: [],
+  imports: [RouterOutlet, FormsModule, CommonModule, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit{
   title = 'InnovativePro-app';
+  prompt = '';
+  response = '';
+  
+  constructor(private http : HttpClient) {
+
+  }
+  ngOnInit() : void {
+    this.sendPrompt();
+  }
+
+  public sendPrompt() {
+
+    this.http.post<any>('http://localhost:3000', { prompt: this.prompt })
+    .subscribe(resp => {
+      console.log(resp);
+      const output = resp.output.join('');
+      this.response = output;
+    },
+    error => {
+      console.error('There was an error!', error);
+    });
+  }
 }
